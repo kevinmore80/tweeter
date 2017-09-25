80b97b0ee0f50cc33d632f08ab261d516092b78a
kevinmore80 <kevinmore@outlook.com>
2017-09-25 02:38:41 +0000

    Switched to using Mongodb

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,16 @@
-$(function() {
+$(document).ready(function() {
 
-  $(".new-tweet textarea").on("keyup", function() {
-    var $input = $(this);
-    var $tweetLength = $input.val().length;
-    var $counter = $input.siblings("span.counter")
+    $('.new-tweet textarea').on('keydown', function() {
+        //this points to the textarea DOM object
+        var $remainingChars = 140 - $(this).val().length;
 
-    $counter.text(140 - $tweetLength);
-    if($tweetLength > 140) {
-      $counter.addClass("invalid");
-    } else {
-      $counter.removeClass("invalid");
-    };
-  });
+        //Print in red if character limit is exceeded
+        if($remainingChars < 0) {
+            $(this).closest('form').find('.counter').text($remainingChars).addClass('red-text');
+        }
+        else {
+            $(this).closest('form').find('.counter').text($remainingChars).removeClass('red-text');
+        }
+
+    });
 });
--- server/index.js
+++ server/index.js
@@ -6,28 +6,31 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const { MongoClient } = require('mongodb');
+const MONGODB_URI   = "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// The database of tweets.
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+// const db = require("./lib/in-memory-db");
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+MongoClient.connect('mongodb://localhost:27017/tweeter', (err, db) => {
 
-// Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+  if (err) {
+    console.log(`Failed to connect to mongodb`);
+    throw err;
+  }
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+
+  const DataHelpers = require("./lib/data-helpers.js")(db);
+  const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+  // Mount the tweets routes at the "/tweets" path prefix:
+  app.use("/tweets", tweetsRoutes);
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
